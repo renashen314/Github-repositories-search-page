b3d8d53c072e2478cab65803a05aa3c04fdbec52
Rena Shen <renashen314@hotmail.com>
2026-03-10 23:09:30 -0400

    use search params

--- src/hooks/useFetchRepo.ts
+++ src/hooks/useFetchRepo.ts
@@ -13,11 +13,17 @@ const useFetchRepo = ({
   sortBy,
   orderBy,
 }: fetchParams) => {
+  const params = new URLSearchParams({
+    q: searchTerm,
+    per_page: itemsPerPage.toString(),
+    sort: sortBy,
+    order: orderBy,
+  });
   const query = useQuery({
     queryKey: ["repositories", [searchTerm, itemsPerPage, sortBy, orderBy]],
     queryFn: async () => {
       const response = await fetch(
-        `https://api.github.com/search/repositories?q=${searchTerm}&per_page=${itemsPerPage}&sort=${sortBy}&order=${orderBy}`,
+        `https://api.github.com/search/repositories?${params}`,
       );
       if (!response.ok) throw new Error("Network response was not ok");
       return response.json();
